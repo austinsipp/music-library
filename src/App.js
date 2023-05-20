c737fd84738154eb72463543fa0ba20fdf1f9b4e
austinsipp <austin.sipp@gmail.com>
2023-05-20 11:46:25 -0700

    updated with context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useRef} from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import { DataContext } from './contexts/DataContext'
+import { SearchContext } from './contexts/SearchContext'
+
+const API_URL = 'https://itunes.apple.com/search?term='
 
 function App() {
+
+  let [message, setMessage] = useState('Search for Music')
+  let [data, setData] = useState([])
+  let searchInput = useRef('')
+
+
+
+  const handleSearch = (e, search) => {
+    e.preventDefault()
+    const fetchData = async () => {
+      try {
+        document.title = `${search} Music`
+        const response = await fetch(`${API_URL}${search}`)
+        const resData = await response.json()
+        console.log(resData)
+        if(resData.results.length) {
+        setData(resData.results)
+        setMessage('')
+      } else {
+        setMessage(`We could find nothing for '${search}'`)
+      }
+      } catch(e) {
+        console.log(e)
+      }
+    }
+    if (search) {
+      fetchData()
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchContext.Provider value={
+        {
+          term: searchInput,
+          handleSearch
+        }
+      }>
+        <SearchBar />
+      </SearchContext.Provider>
+      
+      {message}
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
+      
+
     </div>
   );
 }
